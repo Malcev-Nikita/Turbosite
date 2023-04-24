098ab37f4f5c33565b7d66786c88c4fb8302121f
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-04-24 15:56:15 +0400

    commit 24.4.23 (2)

--- js/video_full.js
+++ js/video_full.js
@@ -1,8 +1,9 @@
 const video = document.querySelectorAll('.video')
+const SliderInnerRight = document.querySelectorAll('.slider_inner_right')[0]
 
 video.forEach(element => {
     
-    element.querySelector('img').addEventListener('click', (evet) => {
+    element.querySelector('img').addEventListener('click', () => {
         element.querySelector('.video_full').classList.add("video_full_active")
     })
 
@@ -10,3 +11,12 @@ video.forEach(element => {
         element.querySelector('.video_full').classList.remove("video_full_active")
     })
 });
+
+
+SliderInnerRight.querySelector('.video__header').addEventListener('click', () => {
+    SliderInnerRight.querySelector('.video_full').classList.add("video_full_active")
+})
+
+SliderInnerRight.querySelector('.video_full img').addEventListener('click', () => {
+    SliderInnerRight.querySelector('.video_full').classList.remove("video_full_active")
+})
