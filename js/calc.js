7169a2f602a844fed5a6f4566089d70967120f1e
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-05-02 11:25:47 +0400

    commit 2.4.23 (1)

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,11 @@
-const button = document.querySelectorAll('.zakaz')
-const calc = document.querySelector('.calc')
-const calcClose = document.querySelector('.calc_close')
+const zakazTurbosite = document.querySelectorAll('.zakaz__turbosite')
+const zakazTurbosite2 = document.querySelectorAll('.zakaz__turbosite2')
+const turbosite = document.querySelector('.turbosite')
+const turbosite2 = document.querySelector('.turbosite2')
+const calcClose = document.querySelectorAll('.calc_close')
 
-let price = 34999
+let priceTurbosite = 24999
+let priceTurbosite2 = 45099
 const priceList = [
     5000,
     15000, 
@@ -15,38 +18,54 @@ const priceList = [
     5000
 ]
 const formCheckInput = document.querySelectorAll('.form-check-input')
-const calcPrice = document.querySelector('.calc__price')
+const calcPriceTurbosite = document.querySelector('.calc__price_turbosite')
+const calcPriceTurbosite2 = document.querySelector('.calc__price_turbosite2')
 
 
 const promocode = 'test'
-const calcPromo = document.querySelector('.calc__promo')
-const calcPromoButton = document.querySelector('.calc__promo_button')
-const calcPromoNotification = document.querySelector('.calc__promo_notification')
+const calcPromo = document.querySelector('.calc__promo_input_turbosite')
+const calcPromo2 = document.querySelector('.calc__promo_input_turbosite2')
+const calcPromoTurbosite = document.querySelector('.calc__promo_turbosite')
+const calcPromoTurbosite2 = document.querySelector('.calc__promo_turbosite2')
+const calcPromoNotificationTurbosite = document.querySelector('.calc__promo_notification_turbosite')
+const calcPromoNotificationTurbosite2 = document.querySelector('.calc__promo_notification_turbosite2')
+
+let promocodeUse1 = false
+let promocodeUse2 = false
 
 
-calcPrice.innerHTML = price
-
-Array.from(button).forEach(element => {
+Array.from(zakazTurbosite).forEach(element => {
     element.addEventListener('click', () => {
-        calc.classList.add('calc_active')
+        calcPriceTurbosite.innerHTML = priceTurbosite
+        turbosite.classList.add('calc_active')
     })
 })
 
-calcClose.addEventListener('click', () => {
-    calc.classList.remove('calc_active')
+Array.from(zakazTurbosite2).forEach(element => {
+    element.addEventListener('click', () => {
+        calcPriceTurbosite2.innerHTML = priceTurbosite2
+        turbosite2.classList.add('calc_active')
+    })
+})
+
+Array.from(calcClose).forEach(element => {
+    element.addEventListener('click', () => {
+        turbosite.classList.remove('calc_active')
+        turbosite2.classList.remove('calc_active')
+    })
 })
 
 
 Array.from(formCheckInput).forEach(element => {
     element.addEventListener('click', () => {
         if (element.checked) {
-            let prePrice = price
-            let lastPrice = price + priceList[Number(element.id.substring(9)) - 1]
+            let prePrice = priceTurbosite
+            let lastPrice = priceTurbosite + priceList[Number(element.id.substring(9)) - 1]
 
-            price = lastPrice
+            priceTurbosite = lastPrice
 
             anime({
-                targets: calcPrice,
+                targets: calcPriceTurbosite,
                 innerHTML: [prePrice, lastPrice],
                 color: ['#30E784', '#0B132A'],
                 round: 1,
@@ -55,13 +74,48 @@ Array.from(formCheckInput).forEach(element => {
             });
         } 
         else {
-            let prePrice = price
-            let lastPrice = price - priceList[Number(element.id.substring(9)) - 1]
+            let prePrice = priceTurbosite
+            let lastPrice = priceTurbosite - priceList[Number(element.id.substring(9)) - 1]
 
-            price = lastPrice
+            priceTurbosite = lastPrice
 
             anime({
-                targets: calcPrice,
+                targets: calcPriceTurbosite,
+                innerHTML: [prePrice, lastPrice],
+                color: ['#FF0000', '#0B132A'],
+                round: 1,
+                easing: 'easeOutQuad',
+                duration: 800
+            });
+        } 
+    })
+})
+
+Array.from(formCheckInput).forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.checked) {
+            let prePrice = priceTurbosite2
+            let lastPrice = priceTurbosite2 + priceList[Number(element.id.substring(9)) - 1]
+
+            priceTurbosite2 = lastPrice
+
+            anime({
+                targets: calcPriceTurbosite2,
+                innerHTML: [prePrice, lastPrice],
+                color: ['#30E784', '#0B132A'],
+                round: 1,
+                easing: 'easeOutQuad',
+                duration: 800
+            });
+        } 
+        else {
+            let prePrice = priceTurbosite2
+            let lastPrice = priceTurbosite2 - priceList[Number(element.id.substring(9)) - 1]
+
+            priceTurbosite2 = lastPrice
+
+            anime({
+                targets: calcPriceTurbosite2,
                 innerHTML: [prePrice, lastPrice],
                 color: ['#FF0000', '#0B132A'],
                 round: 1,
@@ -73,22 +127,44 @@ Array.from(formCheckInput).forEach(element => {
 })
 
 
-calcPromoButton.addEventListener('click', () => {
-    if (calcPromo.value == promocode) {
-        price *= 0.9
+calcPromoTurbosite.addEventListener('click', () => {
+    if (calcPromo.value == promocode && !promocodeUse1) {
+        priceTurbosite *= 0.9
+        promocodeUse1 = true
 
         for (let i = 0; i < priceList.length; i++) {
             priceList[i] *= 0.9
         }  
 
         anime({
-            targets: calcPromoNotification,
+            targets: calcPromoNotificationTurbosite,
+            width: [0, 200],
+            easing: 'easeOutQuad',
+            duration: 200
+        });
+        
+        calcPromoNotificationTurbosite.innerHTML = 'Ваша скидка 10%'
+        calcPriceTurbosite.innerHTML = Math.round(priceTurbosite)
+    }   
+})
+
+calcPromoTurbosite2.addEventListener('click', () => {
+    if (calcPromo2.value == promocode && !promocodeUse2) {
+        priceTurbosite2 *= 0.9
+        promocodeUse2 = true
+
+        for (let i = 0; i < priceList.length; i++) {
+            priceList[i] *= 0.9
+        }  
+
+        anime({
+            targets: calcPromoNotificationTurbosite2,
             width: [0, 200],
             easing: 'easeOutQuad',
             duration: 200
         });
 
-        calcPromoNotification.innerHTML = 'Ваша скидка 10%'
-        calcPrice.innerHTML = Math.round(price)
+        calcPromoNotificationTurbosite2.innerHTML = 'Ваша скидка 10%'
+        calcPriceTurbosite2.innerHTML = Math.round(priceTurbosite2)
     }   
 })
